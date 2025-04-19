f158cfd23b90ec8b81022260e93a04d0d00f1758
suheng <suheng114514@gmail.com>
2025-04-18 20:31:27 -0700

    pack into exe

--- electron/main.ts
+++ electron/main.ts
@@ -5,17 +5,31 @@ import {spawn} from 'node:child_process'
 const { app, BrowserWindow } = await import('electron')
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
+const isPackaged = app.isPackaged
 
-const py = spawn('python',['../backend/server.py'])
+function launchFlaskBackend() {
 
-py.stdout.on('data', (data) => {
-    console.log(`[flask] ${data}`)
-})
+    let pyProcess
 
-py.stderr.on('data', (data) => {
-    console.error(`[flask error] ${data}`)
-})
+    if (isPackaged) {
+        const exePath = path.join(process.resourcesPath, 'server.exe')
+        pyProcess = spawn(exePath)
+    } else {
+        pyProcess = spawn('./backend/.venv/Scripts/python.exe', ['./backend/server.py'])
+    }
 
+    pyProcess.stdout.on('data', (data) => {
+        console.log(`[Flask stdout]: ${data}`)
+    })
+
+    pyProcess.stderr.on('data', (data) => {
+        console.error(`[Flask stderr]: ${data}`)
+    })
+
+    pyProcess.on('error', (err) => {
+        console.error('❌ Failed to start Flask server:', err)
+    })
+}
 function createWindow() {
     const win = new BrowserWindow({
         width: 1000,
@@ -29,11 +43,12 @@ function createWindow() {
             webSecurity: false
         }
     })
-
+    win.webContents.openDevTools()
     win.loadFile(path.join(__dirname, '../frontend/dist/index.html'))
 }
 
 app.whenReady().then(() => {
+    launchFlaskBackend()
     createWindow()
 
     app.on('activate', () => {
@@ -44,3 +59,4 @@ app.whenReady().then(() => {
 app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') app.quit()
 })
+
